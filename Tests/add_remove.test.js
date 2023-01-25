c8b3af126ee07fe7f43ff167246328b26b2e7d61
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-01-25 20:05:23 +0300

    Add test folde for unit testing

---
+++ Tests/add_remove.test.js
@@ -0,0 +1,11 @@
+const addTodo = require("../src/modules/add");
+describe("tests functions responsible for adding todos", () => {
+
+  test("check if addTodo exists", () => {
+    expect(addTodo).toBeDefined();
+  });
+
+  test("check if function adds todo", () => {
+    
+  })
+});
