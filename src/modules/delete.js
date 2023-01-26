834289e2f0dc5ecc3abec82844b6e13bd8691404
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-01-26 11:27:32 +0300

    Update codes for testing

--- src/modules/delete.js
+++ src/modules/delete.js
@@ -2,21 +2,31 @@ import renderTodos from './todo.js';
 import saveTodo from './save.js';
 import getTodos from './getTask.js';
 
-const deleteTodo = () => {
-  const trashIcons = document.querySelectorAll('.trash-icon');
-  trashIcons.forEach((trashIcon) => {
-    trashIcon.addEventListener('click', () => {
-      const index = trashIcon.id;
-      let todos = getTodos().filter((todo) => todo.index !== Number(index));
-      todos = todos.map((todo, i) => {
-        todo.index = i + 1;
-        return todo;
-      });
 
-      saveTodo(todos);
-      renderTodos();
+const deleteTodo = (id) => {
+  const index = id;
+  const todos = getTodos().filter((todo) => todo.index !== Number(index))
+    .map((todo, i) => {
+      todo.index = i - 1;
+      return todo;
     });
-  });
+
+  saveTodo(todos);
+  renderTodos();
 };
 
-export default deleteTodo;
+const deleteTask = ({ target }) => {
+  const index = target.closest('svg').id;
+  deleteTodo(index);
+};
+
+const eventDelete = (trashIcon) => {
+  trashIcon.addEventListener('click', deleteTask);
+};
+const deleteListener = () => {
+  const trashIcons = document.querySelectorAll('.trash-icon');
+  trashIcons.forEach(eventDelete);
+};
+
+export default deleteListener;
+export { deleteTodo };
