6a175d906314122a73c97964146dde76ef1d9f92
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-01-27 14:18:21 +0300

    Updated code for testing

--- src/modules/clear.js
+++ src/modules/clear.js
@@ -1,16 +1,16 @@
-import renderTodos from './todo';
-import saveTodo from './save';
-import getTodos from './getTask';
+import renderTodos from "./todo";
+import saveTodo from "./save";
+import getTodos from "./getTask";
 
 const completedTest = (id) => {
-  const index = Number(id);
+  const index = id;
   const todos = getTodos();
 
-  if(todos[index -1].completed) {
+  if (todos[index - 1].completed) {
     todos[index - 1].completed = false;
-  }else {
+  } else {
     todos[index - 1].completed = true;
-  };
+  }
 
   saveTodo(todos);
   renderTodos();
@@ -21,13 +21,13 @@ const markTodo = ({ target }) => {
 };
 
 const completedListener = (checkbox) => {
-  checkbox.addEventListener('click', markTodo);
+  checkbox.addEventListener("click", markTodo);
 };
 
 const checkMarkListener = () => {
-  const checkboxes = document.querySelectorAll('.checkbox');
+  const checkboxes = document.querySelectorAll(".checkbox");
   checkboxes.forEach(completedListener);
 };
 
 export default checkMarkListener;
-export {completedTest};
+export { completedTest };
